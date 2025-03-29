2398f1d4c3b9bc8232624eafdf3889c407878bdf
ibrahim kedir <techofreact@gmail.com>
2025-03-29 11:24:46 +0300

    ft: set up design-system palletes.

---
+++ src/app/fonts/fonts.ts
@@ -0,0 +1,22 @@
+import { Bebas_Neue, Darker_Grotesque, Kode_Mono } from "next/font/google";
+
+export const font_header = Bebas_Neue({
+    subsets: ['latin'],
+    weight: ['400'],
+    variable: '--font-header',
+    display: 'swap',
+});
+
+export const font_accent = Kode_Mono({
+    subsets: ['latin'],
+    weight: ['400', '500', '600', '700'],
+    variable: '--font-font_accent',
+    display: 'swap',
+});
+
+export const body_font = Darker_Grotesque({
+    subsets: ['latin'],
+    weight: ['300', '400', '500', '600', '700', '800', '900'],
+    variable: '--font-font_accent',
+    display: 'swap',
+});
