c9b0f54850f8ca5a17967a2d7d2e18ab89f6707b
ibrahim kedir <techofreact@gmail.com>
2025-03-30 17:51:57 +0300

    wip: card section made static.

--- src/_ui/service.ui.tsx
+++ src/_ui/service.ui.tsx
@@ -28,10 +28,29 @@ export default function Services() {
         height={0}
         className="w-full aspect-auto absolute bottom-0"
       />
-      <SystemofTheFuture/>
-              <Container className="max-md:max-w-full w-full text-base-0  border-t-1 border-primary-400 md:py-[150px] bg-transparent">
-                <h3 className={`${font_accent.className} text-2xl font-bold mb-16`}>[Services]</h3>
-                <CardGrid/>
+      <SystemofTheFuture />
+      <Container className="max-md:max-w-full w-full text-base-0  border-t-1 border-primary-400 py-[90px] md:py-[150px] bg-transparent">
+        <h3
+          className={`${font_accent.className} text-2xl md:text-4xl font-bold mb-16`}
+        >
+          [Services]
+        </h3>
+        <div className="flex max-lg:flex-wrap max-lg:justify-center grow">
+          {Array(5)
+            .fill(0)
+            .map((_, key) => (
+              <article
+                key={key}
+                className=" bg-base-0 max-md:not-last:border-b border-primary-200 px-6 py-6 drop-shadow-sm shadow-[#2D2E831A] max-md:w-full h-[200px] md:h-[480px]"
+              >
+                <p
+                  className={`${font_header.className} uppercase text-3xl text-primary-600 font-regular`}
+                >
+                  Software Development
+                </p>
+              </article>
+            ))}
+        </div>
       </Container>
     </Container>
   );
