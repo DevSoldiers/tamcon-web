66821ac43b6cd16be7d09d918b543881b7b0a64e
Ibrahim Kedir <techofreact@gmail.com>
2025-04-08 15:22:06 +0300

    ft: footer links with assets.

---
+++ src/_content/footer_content.ts
@@ -0,0 +1,5 @@
+export const email = "CONTACT@TAMCON.COM";
+export const address = "Bedesta Building 9th Floor, Dembel Bole, Addis Ababa";
+export const phoneNumber = "+2519 0000 0000";
+export const footer_nav_links = ["About Us", "LinkedIn", "Services", "Projects", "X"];
+export const copyright_note = "&#169; 2025, Tamcon Software Solutions";
---
+++ src/_ui/footer.tsx
@@ -0,0 +1,46 @@
+import {
+  address,
+  email,
+  footer_nav_links,
+  phoneNumber,
+} from "@/_content/footer_content";
+import Container from "../_components/container";
+import { font_accent, font_body } from "@/app/fonts/fonts";
+import Link from "next/link";
+
+export default function Footer() {
+  return (
+    <Container className="mt-[71.5px] md:mt-[93.5px]">
+      <footer className="grid md:grid-cols-2 justify-baseline md:justify-center">
+        <article className="mb-9 md:mb-8 address_wrapper grid gap-y-1">
+          <p
+            className={`${font_accent.className} text-4xl font-semibold text-gray-600 mb-6`}
+          >
+            {email}
+          </p>
+          <p
+            className={`${font_body.className} text-base font-medium text-gray-600 mb-3`}
+          >
+            {address}
+          </p>
+          <p
+            className={`${font_body.className} text-base font-medium text-gray-600`}
+          >
+            {phoneNumber}
+          </p>
+        </article>
+        <article className="links_wrapper grid grid-cols-2">
+          {footer_nav_links?.map((link, key) => (
+            <Link
+              key={key}
+              href={"/"}
+              className={`${font_accent.className} text-base font-regular text-gray-600`}
+            >
+              {link}
+            </Link>
+          ))}
+        </article>
+      </footer>
+    </Container>
+  );
+}
