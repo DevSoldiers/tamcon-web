3f31c48cc2c2ecb01f4ff7878037038413529eb0
Ibrahim Kedir <techofreact@gmail.com>
2025-04-09 17:18:09 +0300

    ft: services page and asset files.

---
+++ src/_ui/(services)/faq.tsx
@@ -0,0 +1,31 @@
+import Container from "@/_components/container";
+import { font_accent, font_body } from "@/app/fonts/fonts";
+import { faqs } from "@/_content/service_page_content";
+import { Accordion } from "@/_components/accordion";
+
+export default function Faq() {
+  return (
+    <Container className="max-w-full bg-primary-25 py-16 md:py-[120px]">
+      <Container className="px-0 max-w-[1072px]">
+        <h1
+          className={`${font_accent.className} text-4xl font-bold text-primary-600 text-center`}
+        >
+          <span>[</span>
+          FAQS
+          <span>]</span>
+        </h1>
+
+        {faqs?.map((faq, key) => (
+          <Accordion
+            key={key}
+            title={faq.question}
+            titleClassName={`${font_body.className} text-2xl md:text-3xl font-bold text-gray-600 cursor-pointer`}
+            contentClassName={`${font_body.className} text-xl md:text-2xl text-gray-500`}
+          >
+            {faq.answer}
+          </Accordion>
+        ))}
+      </Container>
+    </Container>
+  );
+}
--- src/_ui/(services)/hero.tsx
+++ src/_ui/(services)/hero.tsx
@@ -2,7 +2,7 @@ import Container from "@/_components/container";
 import { font_accent, font_body, font_header } from "@/app/fonts/fonts";
 import Image from "next/image";
 
-export default function Services() {
+export default function ServicesHero() {
   return (
     <Container className="pt-[90.09px] md:pt-20">
       <h1
@@ -20,29 +20,6 @@ export default function Services() {
         businesses of all sizes.
         <span>]</span>
       </h1>
-      {/* software development */}
-      <Container className="bg-primary-25 py-4 md:py-[28.28px] grid gap-y-[22.28px ] md:grid-cols-2 mt-[90px] md:mt-16">
-        <p
-          className={`${font_accent.className} text-2xl md:text-5xl font-medium text-primary-600`}
-        >
-          Software Development
-        </p>
-        <article className="dev-desc flex flex-col gap-y-[27.27px] md:gap-y-[60.27px] justify-center items-center mt-[22.28px] md:mt-0">
-          <Image
-            alt="tamcon software solutions software development icon"
-            src={"/others/sw_development.svg"}
-            width={90}
-            height={82}
-          />
-          <p
-            className={`${font_body.className} text-xs md:text-base font-medium text-primary-600 text-right`}
-          >
-            At Tamcon, we develop custom software solutions designed to meet
-            your specific needs, ensuring scalability, security, and seamless
-            performance across platforms.
-          </p>
-        </article>
-      </Container>
     </Container>
   );
 }
