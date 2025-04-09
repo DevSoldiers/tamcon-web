b5c46bacb6abb7aa9d84c5deb7240807d2c2a07b
Ibrahim Kedir <techofreact@gmail.com>
2025-04-09 11:49:39 +0300

    ft: the team page with wip: circle animation at the bottom.

--- src/_content/footer_content.ts
+++ src/_content/footer_content.ts
@@ -1,5 +1,5 @@
 export const email = "CONTACT@TAMCON.COM";
 export const address = "Bedesta Building 9th Floor, Dembel Bole, Addis Ababa";
 export const phoneNumber = "+2519 0000 0000";
-export const footer_nav_links = ["About Us", "LinkedIn", "Services", "Projects", "X"];
+export const footer_nav_links = ["About Us", "LinkedIn", "Services", "Facebook", "Projects", "X"];
 export const copyright_note = "2025, Tamcon Software Solutions";
